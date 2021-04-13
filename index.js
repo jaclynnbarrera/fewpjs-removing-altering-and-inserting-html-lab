c755f62520ef3b4c94c110daa4dc183f1a0442a3
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-04-13 17:05:45 -0400

    commit tree 78528b512d543c68f1ccf56377a6fef862936698

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById("main").remove()
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = `JAC-B is the champion.`
